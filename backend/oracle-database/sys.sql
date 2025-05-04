620f9a973218452fdd68de18209f980b8979c6f6
jeancdev <jcode2006@gmail.com>
2025-05-04 14:50:42 -0500

    feat: :sparkles: remove obsolete database schema, drop scripts, and triggers

--- backend/oracle-database/sys.sql
+++
@@ -1,9 +0,0 @@
-alter session set "_ORACLE_SCRIPT" = true;
-
--- Crear usuario para la panadería
-create user panaderiadb identified by 1234;
-grant connect,resource to panaderiadb;
-alter user panaderiadb
-   quota unlimited on users;
-
-drop user panaderiadb cascade;
