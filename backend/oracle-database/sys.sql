08cbe0b855e8f6730e252bfafefce2defc57c906
jeancdev <jcode2006@gmail.com>
2025-05-04 14:48:08 -0500

    refactor: :recycle: streamline SQL script for 'panaderiadb' user creation and management

--- backend/oracle-database/sys.sql
+++ backend/oracle-database/sys.sql
@@ -1,26 +1,9 @@
--- Active: 1745851414825@@127.0.0.1@1521@XE@PANADERIADB
-ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+alter session set "_ORACLE_SCRIPT" = true;
 
 -- Crear usuario para la panadería
-CREATE USER panaderiadb IDENTIFIED BY 1234
-DEFAULT TABLESPACE USERS
-TEMPORARY TABLESPACE TEMP
-QUOTA UNLIMITED ON USERS;
+create user panaderiadb identified by 1234;
+grant connect,resource to panaderiadb;
+alter user panaderiadb
+   quota unlimited on users;
 
--- Otorgar los privilegios necesarios
-GRANT CREATE SESSION TO panaderiadb;
-GRANT CREATE TABLE TO panaderiadb;
-GRANT CREATE SEQUENCE TO panaderiadb;
-GRANT CREATE PROCEDURE TO panaderiadb;
-GRANT CREATE TRIGGER TO panaderiadb;
-GRANT CREATE VIEW TO panaderiadb;
-GRANT CREATE SYNONYM TO panaderiadb;
-
--- Otorgar roles básicos para desarrollo
-GRANT CONNECT, RESOURCE TO panaderiadb;
-
--- eliminar el usuario panaderiadb y todas sus dependencias
-DROP USER panaderiadb CASCADE;
-
--- mostrar todos los usuarios de la base de datos
-SELECT username FROM all_users;
+drop user panaderiadb cascade;
